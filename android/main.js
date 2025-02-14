1fc0fb94e95ce313a412af898cfb3fd1910428d6
solraczo <79017916+solraczo@users.noreply.github.com>
2025-02-14 09:22:28 -0500

    Update main.js

--- android/main.js
+++ android/main.js
@@ -143,11 +143,11 @@ renderer.setAnimationLoop((timestamp, frame) => {
 
     // Actualizar el factor de mezcla de texturas (más lento)
     if (increasing) {
-        mixFactor += 0.001; // Cambiado de 0.01 a 0.001 para una transición más lenta
+        mixFactor += 0.0015; // Cambiado de 0.01 a 0.001 para una transición más lenta
         if (mixFactor >= 1.0) increasing = false;
     } else {
-        mixFactor -= 0.001; // Cambiado de 0.01 a 0.001 para una transición más lenta
-        if (mixFactor <= 0.0) increasing = true;
+        mixFactor -= 0.0015; // Cambiado de 0.01 a 0.001 para una transición más lenta
+        if (mixFactor <= 1.0) increasing = true;
     }
 
     // Pasar el valor de mixFactor al shader
