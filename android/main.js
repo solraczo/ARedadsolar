f2df412cb53d00c806bd77bafad6942e9c00dc4b
solraczo <79017916+solraczo@users.noreply.github.com>
2025-02-15 13:18:59 -0500

    Update main.js

--- android/main.js
+++ android/main.js
@@ -63,7 +63,7 @@ rgbeLoader.load(
 // Cargar el modelo GLTF y activar todas sus animaciones en loop
 const gltfLoader = new GLTFLoader();
 gltfLoader.load(
-    'https://solraczo.github.io/ARedadsolar/android/models/edadsolar_9.gltf',
+    'https://solraczo.github.io/ARedadsolar/android/models/edadsolar_10.gltf',
     (gltf) => {
         const model = gltf.scene;
         model.scale.set(0.5, 0.5, 0.5);
